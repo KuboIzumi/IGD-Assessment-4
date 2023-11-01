4a32c639df18c3579506cb71904d73c7d7ccc48c
BrendanLeung <brendan.leung@student.uts.edu.au>
2023-11-02 00:56:56 +1100

    1.1

---
+++ Assets/Scripts/Level1Controller.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level1Controller : MonoBehaviour
+{
+    public void PreviousScene()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+}
